85e68b4e33fab3a72151c435aa93a85ccc9e7cf1
UEM <aniketpaul692@gmail.com>
2025-03-20 15:14:57 +0530

    third

--- swap.java
+++ swap.java
@@ -1,34 +1,13 @@
 import java.util.Scanner;
 
-public class palindrome
-{
-public static boolean checkPalindrome(int n)
-{
-	
-	int temp=n;
-	int sum=0;
-	boolean b;
-	while(n!=0)
-	{
-		int rem=n%10;
-		sum+=rem;
-		n=n/10;
-	}
-	if(temp==sum)
-	{
-		b=true;
-	}
-	else
-	{
-		b=false;
-	}
-	return b;
-
-
+public class reverse {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter the number:");
 	int n=sc.nextInt();
-	System.out.println(checkPalindrome(n));
+	for(int i=n-1;i>0;i--)
+         {
+	System.out.println(i);
+        }
 }
 }
